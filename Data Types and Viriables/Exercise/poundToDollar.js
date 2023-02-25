c89442cac29b2f17e41a0041464577e60bf55303
Aleksandra Adelinova Valchanova <115665658+avalchanova@users.noreply.github.com>
2023-02-25 15:57:32 +0200

    Add files via upload

---
+++ Data Types and Viriables/Exercise/poundToDollar.js
@@ -0,0 +1,5 @@
+function poundToDollar(pound) {
+    let dollar = pound * 1.31;
+    console.log(dollar.toFixed(3));
+}
+poundToDollar(39);
---
+++ Data Types and Viriables/Exercise/reversedChar.js
@@ -0,0 +1,5 @@
+function reversedChar(ch1, ch2, ch3) {
+    console.log(ch3 + " " + ch2 + " " + ch1);
+
+}
+reversedChar('A', 'B', 'C');
