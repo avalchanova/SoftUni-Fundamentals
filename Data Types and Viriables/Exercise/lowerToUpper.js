9ec4f5345a41d2fd3b5830f63371ebd0b818c5bf
Aleksandra Adelinova Valchanova <115665658+avalchanova@users.noreply.github.com>
2023-02-25 15:56:55 +0200

    Add files via upload

---
+++ Data Types and Viriables/Exercise/lowerToUpper.js
@@ -0,0 +1,8 @@
+function lowerToUpper(symbol) {
+    if (symbol === symbol.toLowerCase()) {
+        console.log("lower-case");
+    } else {
+        console.log(`upper-case`);
+    }
+}
+lowerToUpper('L');
