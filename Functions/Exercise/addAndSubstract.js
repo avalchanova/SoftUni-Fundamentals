91093d0fea624ec761d646fcb3ce7a68652d5771
Aleksandra Adelinova Valchanova <115665658+avalchanova@users.noreply.github.com>
2023-03-08 13:20:35 +0200

    Add files via upload

---
+++ Functions/Exercise/addAndSubstract.js
@@ -0,0 +1,16 @@
+function addAndSubstract(a, b, c) {
+
+    let sum = add(a, b);
+    let finalRes;
+    console.log(substract(c));
+
+    function add(a, b) {
+        return (a + b);
+    }
+
+    function substract(c) {
+        return finalRes = sum - c;
+    }
+
+}
+addAndSubstract(23, 6, 10);
