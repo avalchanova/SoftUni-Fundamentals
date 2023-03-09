a26dc78ad5f05551c723a8893aef7b3d48901761
Aleksandra Adelinova Valchanova <115665658+avalchanova@users.noreply.github.com>
2023-03-09 14:53:47 +0200

    Add files via upload

---
+++ Functions/Exercise/oddAndEvenSum.js
@@ -0,0 +1,17 @@
+function oddAndEvenSum(num) {
+    let oddSum = 0;
+    let evenSum = 0;
+    let numberAsString = num.toString();
+
+    for (let char of numberAsString) {
+        let currentNumber = Number(char);
+
+        if (currentNumber % 2 === 0) {
+            evenSum += currentNumber;
+        } else {
+            oddSum += currentNumber;
+        }
+    }
+    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+}
+oddAndEvenSum(1000435);
