53c0674e0619ff8b11df5d41aa3570e23728f5e8
Aleksandra Adelinova Valchanova <115665658+avalchanova@users.noreply.github.com>
2023-03-18 12:08:56 +0200

    Add files via upload

---
+++ Objects and Classes/personInfoAgain.js
@@ -0,0 +1,12 @@
+function personInfoAgain(firstName, lastName, age) {
+    let person = {};
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = age;
+
+    return person;
+}
+personInfoAgain("George",
+    "Smith",
+    "18"
+);
